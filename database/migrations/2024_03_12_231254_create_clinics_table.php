bd6fb4fc1bcf66270602760192c57a67b46d058c
gabrieladrianofx <gabrieladrianofx@gmail.com>
2024-03-12 22:30:29 -0300

    chore: creating clinical entity schema

--- database/migrations/2024_03_12_231254_create_clinics_table.php
+++ database/migrations/2024_03_12_231254_create_clinics_table.php
@@ -1,11 +1,14 @@
 <?php
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    use HasUuids;
+
     protected $fillable = [
         'CNPJ',
         'name_clinic',
@@ -24,7 +27,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('clinics', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id');
+            $table->string('CNPJ', 14)->unique();
+            $table->string('name_clinic');
+            $table->string('email')->unique();
+            $table->string('street_address');
+            $table->string('city');
+            $table->string('zip_code');
+            $table->tinyText('uf');
+            $table->tinyInteger('ddd')->nullable();
             $table->timestamps();
         });
     }
